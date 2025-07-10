c90efcd1556535ac9c6b36a2f3b73033710ed0a6
Anishnits019 <anish4398nov@gmail.com>
2025-07-10 22:16:10 +0530

    redis chnage to become serveless function

--- BackEnd/config/redis.js
+++ BackEnd/config/redis.js
@@ -1,6 +1,8 @@
 // config/redis.js
-import Redis from 'ioredis';
-
-const redis = new Redis(); // defaults to localhost:6379
+import { Redis } from '@upstash/redis';
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
 
 export default redis;
