9a082f51fddbe735c130d953084c2c42e76155c3
Anishnits019 <anish4398nov@gmail.com>
2025-08-22 00:31:52 +0530

    cors_problem

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -19,13 +19,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Preflight CORS handler (must come before routers)
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  return res.sendStatus(204);
-});
 
 // Use CORS middleware for actual requests
 app.use(
